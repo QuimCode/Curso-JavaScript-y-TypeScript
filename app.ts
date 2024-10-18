fe2f2ceaa6d91534c32eeed44e4d121f5cc889da
QuimCode <quimafoncode@gmail.com>
2024-10-17 23:57:27 -0300

    Practicas_bucles2

---
+++ app.ts
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let frutas: string[] = ["manzana", "banana", "pera", "naranja"];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
