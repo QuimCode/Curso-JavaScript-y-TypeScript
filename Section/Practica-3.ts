4834d1ba1809a2b04e8e13709617fb5469d8a692
QuimCode <quimafoncode@gmail.com>
2024-10-18 23:38:47 -0300

    Practicas 18/10/24

--- Section/Practica-3.ts
+++ Section/Practica-3.ts
@@ -154,3 +154,37 @@ for (let i:number=0; i <= 50;) {
     i += 5;
     console.log('Numero :', i);
 }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let frutas: string[] = ["manzana", "banana", "pera", "naranja"];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++;
+}
+
+
+let numero: number = 5;
+let factorial: number = 1;
+let contador: number = numero;
+
+while (contador > 0) {
+    factorial *= contador;
+    contador--;
+}
+console.log(`El factorial de ${numero} es: ${factorial}`);
+
+let palabra: string = "TypeScript";
+let palabraInvertida: string = "";
+
+for (let i = palabra.length - 1; i >= 0; i--) {
+    palabraInvertida += palabra[i];
+}
+console.log("Palabra invertida:", palabraInvertida);
---
+++ helpers/print-multiplication-table.ts
@@ -0,0 +1,6 @@
+export function printMultiplication(num1:number, num2:number) {
+    let resultado:number;
+    resultado = num1 * num2;
+    console.log('El resultado de la multiplicacion es:', resultado);
+    return resultado;
+}
