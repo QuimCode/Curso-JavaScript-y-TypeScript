6f0f1e2353c57d82bfb44178c3deeb30c19b4146
QuimCode <quimafoncode@gmail.com>
2024-10-20 00:04:28 -0300

    Practicas 19/10/24

--- app.ts
+++ app.ts
@@ -3,4 +3,10 @@ let oneFlower = flowers[0];
 
 let people:string[] = ["Fernando","Luis","Quimey"];
 
-console.log( people[0] );
+for (let x = 0; x <= 2; x++) {
+    console.log( flowers[x] );
+}
+
+for (let y = 0; y <= 5; y++) {
+    console.log(people[y]);
+}
