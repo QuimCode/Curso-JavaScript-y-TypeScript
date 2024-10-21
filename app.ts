1dd96659c50d09097e11ed43ee97ad332ab765d7
QuimCode <quimafoncode@gmail.com>
2024-10-20 21:36:41 -0300

    Practicas 19/10/24

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
 let flowers:string[] = ["Rosa","Liryio","Menta"];
 let oneFlower = flowers[0];
 
-let people:string[] = ["Fernando","Luis","Quimey"];
-let numbersLibrary:number[] = [1,204,13,4,56];
+let employers:string[] = ["Fernando","Luis","Quimey"];
+let salariesPeople: number[] = [1500, 2900, 8500]
+let ahourEmployers:string[] = ["8:AM - 5:PM","10:AM - 7:PM", "8:PM - 5:AM"];
 
+// for (let x = 0; x <= 2; x++) {
+//     console.log( flowers[x] );
+// }
 
-for (let x = 0; x <= 2; x++) {
-    console.log( flowers[x] );
-}
+// for (let y = 0; y <= 5; y++) {
+//     console.log(people[y]);
+// }
 
-for (let y = 0; y <= 5; y++) {
-    console.log(people[y]);
-}
-
-for (let contador:number = 0; contador <= numbersLibrary.length; contador++ ) {
-    console.log(numbersLibrary[contador]);
+for (let contador:number = 0; contador <= 2; contador++ ) {
+    console.log("El empleado " + employers[contador] + " Gana un sueldo neto de: " + salariesPeople[contador] + " Pesos, trabajando la franja horaria de: " + ahourEmployers[contador]);
 }
