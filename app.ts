7bbb70a5d0866dc4bc109b4351a2a7b5b6d58fa9
QuimCode <quimafoncode@gmail.com>
2024-10-20 22:20:38 -0300

    Practicas 19/10/24

--- app.ts
+++ app.ts
@@ -18,90 +18,88 @@
 
 // persona.impresionInfo();
 
+// let auto = {
+//     color: 'Blanco',
+//     traccion: '4x4',
+//     llantas: 4,
+//     puertas: 5,
+//     marca: 'Jeep',
+//     tieneCombustible: true,
+//     añosDeUso: 3,
+//     esNuevo: false,
+// }
 
-let auto = {
-    color: 'Blanco',
-    traccion: '4x4',
-    llantas: 4,
-    puertas: 5,
-    marca: 'Jeep',
-    tieneCombustible: true,
-    añosDeUso: 3,
-    esNuevo: false,
-}
+// let smartTv = {
+//     marca: 'Samsung',
+//     resolucion: '4K Ultra HD',
+//     pantallaTactil: false,
+//     tieneBluetooth: true,
+//     tieneConectividadWifi: true,
+//     tieneAsistenteVirtual: false,
+//     tieneSmartHome: true,
+//     añosDeUso: 0,
+//     esNuevo: true,
+// }
 
-let smartTv = {
-    marca: 'Samsung',
-    resolucion: '4K Ultra HD',
-    pantallaTactil: false,
-    tieneBluetooth: true,
-    tieneConectividadWifi: true,
-    tieneAsistenteVirtual: false,
-    tieneSmartHome: true,
-    añosDeUso: 0,
-    esNuevo: true,
-}
+// let youtubeVideo = {
+//     titulo: 'El camino al éxito',
+//     duracion: '2:30',
+//     nroDeLikes: 10000,
+//     nroDeComentarios: 500,
+//     nroDeVisualizaciones: 500000,
+//     esEnVivo: false,
+//     esRelacionadoCon: ['Cine', 'Deportes', 'Familia'],
+//     esTrending: true,
+//     esPopular: false,
+//     esNovedad: false,
+//     esDeInteres: true,
+// }
 
-let youtubeVideo = {
-    titulo: 'El camino al éxito',
-    duracion: '2:30',
-    nroDeLikes: 10000,
-    nroDeComentarios: 500,
-    nroDeVisualizaciones: 500000,
-    esEnVivo: false,
-    esRelacionadoCon: ['Cine', 'Deportes', 'Familia'],
-    esTrending: true,
-    esPopular: false,
-    esNovedad: false,
-    esDeInteres: true,
-}
+// console.log('=============');
+// console.log(auto);
+// console.log('=============');
+// console.log(smartTv);
+// console.log('=============');
+// console.log(youtubeVideo);
 
-console.log('=============');
-console.log(auto);
-console.log('=============');
-console.log(smartTv);
-console.log('=============');
-console.log(youtubeVideo);
-
-console.log('=============');
+// console.log('=============');
 // ARREGLOS DE OBJETOS
 
-let fernando = {
+interface Personas {
+    nombre: string;
+    apellido: string;
+    edad: number;
+    gustos: string[];
+    esEstudiante: boolean;
+}
+
+let fernando: Personas = {
     nombre: 'Fernando',
     apellido: 'Perez',
     edad: 30,
     gustos: ['Cine', 'Deportes', 'Viajar'],
-    esAmigoDe: ['Maria', 'Pedro', 'Juan'],
-    esAmigaDeMaria: true,
-    esAmigaDePedro: false,
-    esAmigaDeJuan: false,
+    esEstudiante: true,
 }
 
-let luis = {
+let luis: Personas = {
     nombre: 'Luis',
     apellido: 'Gomez',
     edad: 25,
     gustos: ['Musica', 'Deportes', 'Cocinar'],
-    esAmigoDe: ['Maria', 'Fernando', 'Juan'],
-    esAmigaDeMaria: false,
-    esAmigaDeFernando: true,
-    esAmigaDeJuan: true,
+    esEstudiante: true,
 }
 
-let maria = {
+let maria: Personas = {
     nombre: 'Maria',
     apellido: 'Garcia',
     edad: 28,
     gustos: ['Musica', 'Deportes', 'Cocinar'],
-    esAmigoDe: ['Fernando', 'Luis', 'Juan'],
-    esAmigaDeFernando: true,
-    esAmigaDeLuis: true,
-    esAmigaDeJuan: false,
+    esEstudiante: false,
 }
 
-let people = [fernando, maria, luis];
+let people: Personas[] = [fernando, maria, luis];
 
 for (let i = 0; i <= people.length; i++ ) {
     let person = people[i];
-    console.log(person.nombre + person.apellido + ' es ');
+    console.log(person.nombre + ' ' + person.apellido + ' tiene una edad de: ' + person.edad + ' años. Sus gustos son: ' + person.gustos);
 }
