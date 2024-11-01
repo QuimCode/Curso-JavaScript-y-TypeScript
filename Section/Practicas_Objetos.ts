7c85a77bb20bad3f8ccc95cbe6af5bf5d55d0c64
QuimCode <quimafoncode@gmail.com>
2024-11-01 19:03:43 -0300

    Practicas 01/11/24

---
+++ Section/Practicas_Objetos.ts
@@ -0,0 +1,67 @@
+// let persona = {
+//     nombre: 'Maria',
+//     años: 23,
+//     gustos: ["Musica", "Juegos"],
+//     esLatina: true,
+//     estaRegistrada: true,
+
+//     impresionInfo() {
+//         if (persona.estaRegistrada === false) {
+//             console.log('Usted no esta registrada');
+//             return;
+//         } else {
+//             let objectString = persona.nombre + ' Tiene la edad de: ' + persona.años + ' y sus gustos son la ' + persona.gustos;
+//             console.log(objectString);
+//         }
+//     }
+// }
+
+// persona.impresionInfo();
+
+// let auto = {
+//     color: 'Blanco',
+//     traccion: '4x4',
+//     llantas: 4,
+//     puertas: 5,
+//     marca: 'Jeep',
+//     tieneCombustible: true,
+//     añosDeUso: 3,
+//     esNuevo: false,
+// }
+
+// let smartTv = {
+//     marca: 'Samsung',
+//     resolucion: '4K Ultra HD',
+//     pantallaTactil: false,
+//     tieneBluetooth: true,
+//     tieneConectividadWifi: true,
+//     tieneAsistenteVirtual: false,
+//     tieneSmartHome: true,
+//     añosDeUso: 0,
+//     esNuevo: true,
+// }
+
+// let youtubeVideo = {
+//     titulo: 'El camino al éxito',
+//     duracion: '2:30',
+//     nroDeLikes: 10000,
+//     nroDeComentarios: 500,
+//     nroDeVisualizaciones: 500000,
+//     esEnVivo: false,
+//     esRelacionadoCon: ['Cine', 'Deportes', 'Familia'],
+//     esTrending: true,
+//     esPopular: false,
+//     esNovedad: false,
+//     esDeInteres: true,
+// }
+
+// console.log('=============');
+// console.log(auto);
+// console.log('=============');
+// console.log(smartTv);
+// console.log('=============');
+// console.log(youtubeVideo);
+
+// console.log('=============');
+// ARREGLOS DE OBJETOS
+
